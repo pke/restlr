08a4c20e09e580acbdfd83c46f88e81a0ba4d127
Philipp Kursawe <phil.kursawe@gmail.com>
2019-11-11 12:05:56 +0100

    feat: Add pipe input support

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,7 @@
 // eslint-disable-next-line node/shebang
 const { prompt } = require("enquirer")
 const handleRequestAction = require("./handleRequestAction")
+const resourcePrompt = require("./prompts/resourcePrompt")
 
 const cpromise = (promise, defaultResult) => {
   let outerReject
@@ -19,6 +20,11 @@ const cpromise = (promise, defaultResult) => {
   return wrappedPromise
 }
 
+// Using a single function to handle multiple signals
+// Using a single function to handle multiple signals
+function handle(signal) {
+  console.log(`Received ${signal}`)
+}
 
 // https://pastebin.com/raw/B7KHJL4r
 
@@ -26,10 +32,40 @@ function isCancel(e) {
   return e === ""
 }
 
+process.on("exit", (code) => {
+  console.log(`About to exit with code: ${code}`)
+});
+
 (async () => {
   let action = {
     request: {
-      href: "https://restlr.net/discover.json" //"https://api.github.com/users/pke/repos" "https://api.github.com/repos/pke/acts_as_bookable/forks"
+      href: "http://localhost:3000" // "https://restlr.net/discover.json" //"https://api.github.com/users/pke/repos" "https://api.github.com/repos/pke/acts_as_bookable/forks"
+    }
+  }
+
+  if (!process.stdin.isTTY) {
+    process.stdin.resume()
+    
+    process.on("SIGPIPE", handle)
+    process.on("SIGINT", handle)
+    process.on("SIGTERM", handle)
+
+    process.stdin.setEncoding("utf8")
+    
+    const json = await new Promise((resolve, reject) => {
+      var data = ""
+      process.stdin.on("data", function(chunk) {
+        data += chunk
+      })  
+      process.stdin.on("end", () => {
+        console.log("INCOMING: " + data)
+        resolve(JSON.parse(data))
+      })
+      process.stdin.on("error", reject)
+    })
+    
+    action = {
+      prompt: resourcePrompt(json)
     }
   }
 
@@ -50,6 +86,10 @@ function isCancel(e) {
       } else if (action.debug) {
         // Retry the erroneous action by default
         action = action.debug
+        // eslint-disable-next-line no-unused-vars
+        const { request, prompt } = action
+        // You can inspect the action or its already destructured `request` and
+        // `prompt` members to see what might have lead to the error
         // eslint-disable-next-line no-debugger
         debugger
         continue
@@ -67,4 +107,5 @@ function isCancel(e) {
       }
     }
   }while (action)
+  console.log("no more actions")
 })()
