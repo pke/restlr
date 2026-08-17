af79959004297c8f7813589c38d2f672dd91cd59
Philipp Kursawe <phil.kursawe@gmail.com>
2026-08-17 20:19:20 +0200

    fix: update vulnerable dependencies

--- test/title.test.js
+++ test/title.test.js
@@ -4,20 +4,20 @@ const title = require("../src/title")
 
 t.test("when title exists", t => {
   t.plan(1)
-  t.is(title({ title: "The Big Lebowski" }), "The Big Lebowski")
+  t.equal(title({ title: "The Big Lebowski" }), "The Big Lebowski")
 })
 
 t.test("title with default", t => {
   t.plan(1)
-  t.is(title({ }, "The Big Lebowski"), "The Big Lebowski")
+  t.equal(title({ }, "The Big Lebowski"), "The Big Lebowski")
 })
 
 t.test("Default title with rel", t => {
   t.plan(1)
-  t.is(title({ rel: ["index"] }, "The Big Lebowski"), "The Big Lebowski (rel=index)")
+  t.equal(title({ rel: ["index"] }, "The Big Lebowski"), "The Big Lebowski (rel=index)")
 })
 
 t.test("No rel when title", t => {
   t.plan(1)
-  t.is(title({ title: "The Big Lebowski", rel: ["index"] }), "The Big Lebowski")
+  t.equal(title({ title: "The Big Lebowski", rel: ["index"] }), "The Big Lebowski")
 })
